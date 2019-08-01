c1704cfc323c02624098e81f33a99f4afd137130
jackson- <jackson.t.devin@gmail.com>
2019-08-01 12:06:57 -0400

    init commit

---
+++ src/pages/contact.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const ContactPage = () => {
+  return (
+    <div>
+      <h1>Contact Page</h1>
+      <p>Find me at 347-555-555 or info@somewhere.com</p>
+    </div>
+  )
+}
+
+export default ContactPage
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,14 @@
 import React from "react"
+import { Link } from "gatsby"
 
-export default () => <div>Hello world!</div>
+const IndexPage = () => {
+  return (
+    <div>
+      <h1>Home Page</h1>
+      <h2>A description for this place</h2>
+      <p><Link to="/contact">Contact Me</Link></p>
+    </div>
+  )
+}
+
+export default IndexPage
